2a2f3d21a45a72ee9df9ad28e215a98a33fef032
Abhishek Kumar Sahu <abhiiii0017@gmail.com>
2023-06-06 15:29:40 +0530

    html pages

--- app.py
+++ app.py
@@ -1,78 +1,55 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
+import pandas as pd
+import numpy as np
 import plotly.express as px
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from werkzeug.utils import secure_filename
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import squarify as sq
+import plotly.io as pio
+pio.templates
+import seaborn as sns
+import resample as rs
 
 app = Flask(__name__)
 
-def getdb():
-    engine = create_engine('sqlite:///project.sqlite')
-    DBSession = sessionmaker(bind=engine)
-    session = scoped_session(DBSession)
-    return session
-
-def load_gapminder():
-    df = px.data.gapminder()
+def load_data():
+    df = pd.read_csv('dataset/funding.csv')
     return df
+
+def load_data1():
+    df_top_industry_vertical = pd.read_csv('dataset/funding.csv')
+    return df_top_industry_vertical
 
 @app.route('/')
 def index():
-    name = "Sample Project One"
-    return render_template('index.html', title=name)
+    return render_template('index.html')
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    df = load_gapminder()
-    fig1 = None
-    if request.method=='POST':
-        country = request.form.get('country')
-        year = request.form.get('year')
-        if len(country) == 0 and len(year) != 0:
-            year = int(year)
-            result = df.query("year == @year")
-            fig1 = px.sunburst(result, path=['continent', 'country'], values='pop')
-            fig1 = fig1.to_html()
-        elif len(year) == 0 and len(country) != 0:
-            result = df.query("country == @country")
-            fig1 = px.area(result, x='year', y='pop')
-            fig1 = fig1.to_html()
-        elif len(country) != 0 and len(year) != 0:
-            year = int(year)
-            result = df.query("country == @country").query("year == @year")
-        else:
-            result = df
-        return render_template('home.html',
-                               result=result.to_html(),
-                               fig1 = fig1)
-    return render_template('home.html')
+@app.route('/graphs')
+def graphs():
+   df=load_data()
+   fig1 = px.pie(df, values='Amount in USD', names='Industry Vertical', title='Amount in USD by Industry Vertical',
+            color='Industry Vertical',
+            labels={'Industry Vertical':'Industry Vertical'},
+            width=1000, height=600, template='none',
+            color_discrete_sequence=px.colors.sequential.RdBu)
+            fig.update_traces(textposition='inside', textinfo='percent+label', hole=0.4, marker=dict(line=dict(color='#000000', width=2)),
+                   opacity=0.8, rotation=45, textfont_size=15, texttemplate='%{label}<br>%{value:$,.2f}'
+                        and '%{percent}')
+   
+   fig2 = px.histogram(df_top_industry_vertical,
+                    x="Amount in USD", y="Industry Vertical", color="Industry Vertical", hover_name="Startup Name", log_x=True,width=1000, height=600,
+                title="Amount in USD by Industry Vertical", labels={"Amount in USD": "Amount in USD", "Industry Vertical": "Industry Vertical"}, template='plotly_white',
+                color_discrete_sequence=px.colors.qualitative.Dark24, nbins=100, )
+   
+   fig6= px.scatter_polar(df[:70], r="Amount in USD", theta="Industry Vertical", color="Industry Vertical", hover_name="Startup Name", log_r=True, size_max=60,
+                title="Amount in USD by Industry Vertical", labels={"Amount in USD": "Amount in USD", "Industry Vertical": "Industry Vertical"},  width=1000, height=600, symbol="Industry Vertical",
+                color_discrete_sequence=px.colors.qualitative.Dark24, template='none')
+   return render_template('graphs.html',fig1=fig1.html(),fig2=fig2.html(),fig6=fig6)
 
-@app.route('/vehicle/add', methods=['GET', 'POST'])
-def add_vehicle():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        contact = request.form.get('contact')
-        image = request.files.get('file')
-        # validate the data
-        if len(name) == 0 or len(contact) == 0 or image is None:
-            print("Please fill all the fields")
-            return redirect('/vehicle/add')
-        # save the file in a folder
-        filename = secure_filename(image.filename)
-        filepath = 'static/uploads/'+filename
-        image.save(filepath)
-        print('file saved')
-        # save the data in the database
-        db = getdb()
-        db.add(Vehicle(name=name, contact=contact, image=filepath))
-        db.commit()
-        db.close()
-        print('data saved')
-        return redirect('/vehicle/add') 
-
-    return render_template('vehicle.html')
-
+@app.route('/graphs1')
+def 
+         
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000, debug=True)
-
+  app.run(host='127.0.0.1', port=8000, debug=True)
+ 
